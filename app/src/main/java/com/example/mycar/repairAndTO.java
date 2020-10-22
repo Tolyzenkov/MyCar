5e3cc56bc11f1a00ce48de141d37a9a8e1244171
Денис Толызенков <tolyzenkov.dn@yandex.ru>
2020-10-22 19:03:26 +1000

    21.10.2020 19:30

---
+++ app/src/main/java/com/example/mycar/repairAndTO.java
@@ -0,0 +1,14 @@
+package com.example.mycar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class repairAndTO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repair_and_t_o);
+    }
+}
