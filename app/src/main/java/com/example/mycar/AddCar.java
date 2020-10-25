37548a46da7166458b2dd88d1db664fc972aa71b
Денис Толызенков <tolyzenkov.dn@yandex.ru>
2020-10-25 22:09:33 +1000

    21.10.2020 19:30

--- app/src/main/java/com/example/mycar/AddCar.java
+++ app/src/main/java/com/example/mycar/AddCar.java
@@ -1,12 +1,11 @@
 package com.example.mycar;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,56 +15,84 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class AddCar extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class AddCar extends AppCompatActivity {
 
-    Spinner spinner;
-    ArrayList spinnerArrayList;
-    ArrayAdapter spinnerAdapter;
+    Spinner brandSpinner, modelSpinner, fuelSpinner;
+
+    ArrayList brandSpinnerArrayList, modelSpinnerArrayList, fuelSpinnerArrayList;
+
+    ArrayAdapter brandSpinnerAdapter, modelSpinnerAdapter, fuelSpinnerAdapter;
+
     String brand;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
 
-        createBrandSpinner();
+        brandSpinner = (Spinner) findViewById(R.id.spinnerBrand);
+        modelSpinner = (Spinner) findViewById(R.id.modelSpinner);
+        fuelSpinner = (Spinner) findViewById(R.id.typeOfFuel);
+
+        brandSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                ((TextView)brandSpinner.getChildAt(0)).setTextColor(Color.WHITE);
+                ((TextView)brandSpinner.getChildAt(0)).setGravity(Gravity.LEFT);
+                ((TextView)brandSpinner.getChildAt(0)).setTextSize(16);
+                String selectedBrand = brandSpinner.getSelectedItem().toString();
+
+                switch (selectedBrand) {
+                    case "Toyota":
+                        modelSpinner.setAdapter(new ArrayAdapter<String>(AddCar.this, android.R.layout.simple_spinner_dropdown_item,
+                        getResources().getStringArray(R.array.ToyotaModels)));
+                        break;
+
+                    case "Nissan":
+                        modelSpinner.setAdapter(new ArrayAdapter<String>(AddCar.this, android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.NissanModels)));
+                        break;
+
+                    case "Honda":
+                        modelSpinner.setAdapter(new ArrayAdapter<String>(AddCar.this, android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.HondaModels)));
+                        break;
+                    case "Subaru":
+                        modelSpinner.setAdapter(new ArrayAdapter<String>(AddCar.this, android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.SubaruModels)));
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        modelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 
-    public void cancel (View view) {
+    public void cancel(View view) {
         Intent addCarIntent = new Intent(AddCar.this, MainActivity.class);
         startActivity(addCarIntent);
     }
 
-    void createBrandSpinner() {
-        spinner = findViewById(R.id.spinnerBrand);
-        spinner.setOnItemSelectedListener(this);
-        spinnerArrayList = new ArrayList();
-        spinnerArrayList.add("Марка");
-        spinnerArrayList.add("Toyota");
-        spinnerArrayList.add("Nissan");
-        spinnerArrayList.add("Subaru");
-        spinnerArrayList.add("Honda");
 
-        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
-
-    }
-
-
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        ((TextView)spinner.getChildAt(0)).setTextColor(Color.WHITE);
-        ((TextView)spinner.getChildAt(0)).setGravity(Gravity.CENTER_HORIZONTAL);
-        ((TextView)spinner.getChildAt(0)).setTextSize(16);
-        brand = spinner.getSelectedItem().toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
